0b1b0c0ca9541916694dbe9bed38f3ec4d08c51b
mrChernicharo <felipe.chernicharo@gmail.com>
2020-04-20 17:01:09 -0300

    subindo app

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '9cf2fb9c1c9974edce850799e82a81e1',
+    expriresIn: '1d',
+  },
+};
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,43 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import AppError from '../errors/AppError';
+import authConfig from '../config/auth';
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination', 401);
+    }
+    const matchedPassword = await compare(password, user.password);
+
+    if (!matchedPassword) {
+      throw new AppError('Incorrect email/password combination', 401);
+    }
+
+    const { secret, expriresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn: expriresIn,
+    });
+
+    return { user, token };
+  }
+}
+export default AuthenticateUserService;
