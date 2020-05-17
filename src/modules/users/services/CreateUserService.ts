a9f3f58f1afc88dd14d116eaf44baa1a3d09e8ae
mrChernicharo <felipe.chernicharo@gmail.com>
2020-05-17 18:42:21 -0300

    invalidando cache ao criar novo user

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
+import ICacheProvider from '@shared/container/providers/CachePrrovider/models/ICacheProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
@@ -16,7 +17,9 @@ class CreateUserService {
   constructor(
     @inject('UsersRepository') private usersRepository: IUsersRepository,
 
-    @inject('HashProvider') private hashProvider: IHashProvider
+    @inject('HashProvider') private hashProvider: IHashProvider,
+
+    @inject('CacheProvider') private cacheProvider: ICacheProvider
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -31,6 +34,8 @@ class CreateUserService {
       email,
       password: hashedPassword,
     });
+
+    await this.cacheProvider.invalidatePrefix('providers-list');
 
     return user;
   }
